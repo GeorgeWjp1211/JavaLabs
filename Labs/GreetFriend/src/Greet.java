222aa1d5fe76a7699d08be7837eed46e9fd00a5c
George Wang <Georgewang1211@gmail.com>
2023-07-25 23:37:30 +0100

    adding files java

---
+++ Labs/GreetFriend/src/Greet.java
@@ -0,0 +1,6 @@
+public class Greet {
+    public static void main(String[] args) {
+        String friendName = "George";
+        System.out.println("Greetings " + friendName + " from the great class!");
+    }
+}
